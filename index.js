40ae91d63547fd302f34f145b7c3abbcd2e536e3
Joshua Danao <josh@Joshuas-MacBook-Air.local>
2020-06-29 11:53:12 -0700

    added NPM packages for middleware action

---
+++ index.js
@@ -0,0 +1,27 @@
+// Require NPM libraries
+require('dotenv').config();
+const Express = require('express');
+const ejsLayouts = require('express-ejs-layouts');
+const helmet = require('helmet');
+const session = require('express-session');
+const flash = require('flash');
+
+//app setup
+const app = Express();
+app.use(Express.urlencoded({ extended: false}));
+app.use(Express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
+app.use(ejsLayouts);
+app.use(require('morgan')('dev'));
+app.use(helmet());
+
+// ROUTES
+app.get('/', function(req, res) {
+    //check to see if user loggged in
+    res.render('index');
+})
+
+// initialize app on port
+app.listen(process.env.PORT || 3000, function() {
+    console.log(`Listening to the smooth sweet sounds of port ${process.env.PORT} in the morning.`)
+});
