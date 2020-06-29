75a55b8284e9fec37ccb7c70d3c34718dd45fa06
Joshua Danao <josh@Joshuas-MacBook-Air.local>
2020-06-29 14:53:09 -0700

    update

--- models/index.js
+++ models/index.js
@@ -1,1 +1,8 @@
-//handle DB connection/middleware
+// handle DB connection/middleware or apps
+'use strict';
+
+// import filesystem
+// import path 
+// initialize orm layer
+// import env variables
+// initialize db variable
