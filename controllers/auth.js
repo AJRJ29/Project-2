b35c53c270462c1da602150f82256043f63d5414
Joshua Danao <josh@Joshuas-MacBook-Air.local>
2020-06-29 15:52:30 -0700

    added post route for user regis

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,12 +5,39 @@ const router = express.Router();
 // import db
 const db = require('../models')
 // import middleware
+const flash = require('flash');
 
 // register get toute 
 router.get('/register', function(req, res) {
     res.render('auth/register');
 })
 // signup post route 
+router.post('/register', function(req, res) {
+    db.user.findOrCreate({
+        where: {
+            email: req.body.email
+        }, defaults: {
+            name: req.body.name,
+            password: req.body.password
+        }
+    })
+    .then(function([user, created]) {
+        // if user was created 
+        if (created) {
+             // authenticate user and start authorization process
+            console.log("User Created 🏆");
+            res.redirect('/');
+        } else {
+            console.log("User email already exist! 🚁");
+            req.flash('error', 'Error: email already exist for user. Try again');
+            res.redirect('/auth/register')
+        }
+    }).cath(function(err) {
+        console.log(`Error found. \nMessage: ${error.message}. \nPlease review - ${err}`);
+        req.flash('error', err.message);
+        res.redirect('/auth/register')
+    })
+})
 
 // login get route
 router.get('/login', function(req, res) {
